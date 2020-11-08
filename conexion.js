72c5dd4ec8b0cf5074c5af696c2af0fb5fd014bd
Paula Carbone <paulacamilacarbone@gmail.com>
2020-11-08 19:48:31 -0300

    agregando estilo y modificando como trae los elementos

--- conexion.js
+++ conexion.js
@@ -14,13 +14,14 @@ async function buscarImagenes(){
     lista.forEach(element => {
         let div=document.createElement("div");
         div.innerHTML=`
-        <img  src=${element.image} class="card-img-top"></img>
-        <div class="card-body">
-		  <h5 class="card-title">${element.character}</h5>
-		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		  <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </a>
+       
+       <div class = "card" style="width: 18rem">
+                        <img  src=${element.image}class="card-img-top"></img>
+                         
+                        <h1>${element.character}</h1> 
+                        <p>${element.quote}</p>
+                      </div>
+                        
         `
         listadoImg.appendChild(div);
 
